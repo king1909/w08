74255ffef9abca7f25dce24c48e8c1c68b74de0e
work05 <ketsaraporn963@gmail.com>
2024-10-21 15:33:45 +0700

    todo

--- src/Pages/Todo/Todo.jsx
+++ src/Pages/Todo/Todo.jsx
@@ -1,7 +1,13 @@
 import { useState, useEffect, useRef } from 'react'
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { Form } from 'react-bootstrap';
+
 import { fetchTodos } from '../../data/todos'
 
 import './Todo.css'
+
 
 const initItemsPerPage = 5
 const initOnlyWaiting = false
@@ -68,68 +74,147 @@ function Todo() {
 
   // ฟังก์ชั่น waitingClick 
     function waitingClick(id) {
-      const todoSelected = todosRaw.find((todo) => {
-        return todo.id === id
-      })
+      const todoSelected = todosRaw.find((todo) => todo.id === id)
+
       todoSelected.completed = true
       setTodosRaw( [...todosRaw] )
     }
 
+    function addClick(id,title) {
+      const newItem = {
+        id,
+        title,
+        completed: false,
+        userId: 1
+
+      }
+
+      setTodosRaw( [...todosRaw, newItem] )
+    }
+
+
+    //modal handlers
+    const [show, setShow] = useState(false);
+
+
+    const newIdRef = useRef();
+    const newtitleRef = useRef();
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
+
   return (
     <div className='todo-container'>
+
+{/* modal */}
+      <Modal show={show} onHide={handleClose}>
+
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <span className='bi bi-plus-lg'>&nbsp;Add todo</span>
+            </Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>ID:</Form.Label>
+              <Form.Control type="text" autoFocus disabled 
+              //หาค่าตัวเลขมาใส่หาค่ามากสุด แบบauto
+              value={
+                Number(todosRaw.reduce((prev, todo) => {
+                  return todo.id > prev ? todo.id : prev
+                },0)) + 1
+              } 
+              ref={newIdRef}/>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+              <Form.Label>Title:</Form.Label>
+              <Form.Control type="text" autoFocus ref={newtitleRef}/>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            <span className='bi bi-x-lg'>&nbsp;Canael</span>
+          </Button>
+
+          <Button variant="primary" onClick={ () => {
+            //ดึงค่า id title
+            const id = newIdRef.current.value
+            const title = newtitleRef.current.value.trim()
+            if (title === '') {
+              alert('Title cannot be empty')
+              newtitleRef.current.value = ''
+              newtitleRef.current.focus()
+            } else{
+               addClick(id,title)
+            handleClose()
+            }
+          }}>
+           <span className='bi bi-plus-lg'>&nbsp;Add</span> 
+          </Button>
+      </Modal.Footer>
+      </Modal>
+
 {/* filters */}
-      <div className='todo-filters-container'>
+<div className='todo-filters-container'>
+  <div style={{ display: 'flex', alignItems: 'center' }}>
+    {/* form-check form-switch */}
+    <div className='form-check form-switch'>
+      <input
+        className='form-check-input'
+        type='checkbox'
+        role='switch'
+        id='flexSwitchCheckChecked'
+        onChange={(e) => setOnlyWaiting(e.target.checked)}
+        ref={onlyWaitingRef}
+      />
+      <label className='form-check-label' htmlFor='flexSwitchCheckChecked'>
+        Show only&nbsp;
+      </label>
+    </div>
 
-      {/* form-check form-switch */}
-        <div className='form-check form-switch'>
-          <input
-            className='form-check-input'
-            type='checkbox'
-            role='switch'
-            id='flexSwitchCheckChecked'
-            // checked Show only waiting
-            onChange={(e) => {
-              setOnlyWaiting(e.target.checked)
-            }}
-            ref={onlyWaitingRef}
-          />
+    {/* ปุ่ม waiting */}
+    <button className='btn btn-warning' style={{ marginLeft: '10px' }}>
+      waiting&nbsp;
+      <span className='bi bi-clock'></span>
+    </button>
+  </div>
 
-          <label className='form-check-label' htmlFor='flexSwitchCheckChecked'>
-            Show only&nbsp;
-            <button className='btn btn-warning'>
-              waiting&nbsp;
-              <span className='bi bi-clock'></span>
-              </button>
-
-          </label>
-        </div>
-
-        {/* per page */}
-        <select
-          className='form-select'
-          aria-label='Default select example'
-          defaultValue={5}
-          style={{ width: '200px' }}
-          onChange={(e) => {
-            /* console.log(e.target.value) */
-            setItemsPerPage(e.target.value)
-          }}
-          ref={itemsPerPageRef}
-        >
-          <option value={5}>5 items per page</option>
-          <option value={10}>10 items per page</option>
-          <option value={50}>50 items per page</option>
-          <option value={100}>100 items per page</option>
-        </select>
-      </div>
+  {/* per page */}
+  <select
+    className='form-select'
+    aria-label='Default select example'
+    defaultValue={5}
+    style={{ width: '200px' }}
+    onChange={(e) => setItemsPerPage(e.target.value)}
+    ref={itemsPerPageRef}
+  >
+    <option value={5}>5 items per page</option>
+    <option value={10}>10 items per page</option>
+    <option value={50}>50 items per page</option>
+    <option value={100}>100 items per page</option>
+  </select>
+</div>
 
 {/* table */}
-      <table className='table table-striped todo-table'>
+      <table className='table table-striped todo-table' >
         <thead className='table-dark'>
           <tr>
-            <th style={{width:'10%'}}>ID</th>
-            <th>Title</th>
-            <th style={{ textAlign: 'right' }}>Completed</th>
+            <th style={{width:'5%'}} valign = 'middle'>ID</th>
+            <th valign = 'middle'>TITLE</th>
+            <th style={{ textAlign: 'right', width: '20%' }} valign = 'middle'>
+              COMPLETED&nbsp;
+              <button className='btn btn-primary' onClick={() =>  handleShow() }>
+                {/* ADD */}
+                <span className='bi bi-plus-lg'></span>
+                </button>
+            </th>
           </tr>
         </thead>
 
@@ -152,7 +237,7 @@ function Todo() {
           .map((todo) => {
             return (
               <tr key={todo.id}>
-                <td>
+                <td valign = 'middle'>
                   <span
                     className='badge bg-secondary'
                     style={{ width: '3rem' }}
@@ -160,8 +245,9 @@ function Todo() {
                     {todo.id}
                   </span>
                 </td>
-                <td style={{ textAlign: 'left' }}>{todo.title}</td>
-                <td style={{ textAlign: 'right' }}>
+                <td style={{ textAlign: 'left' }} valign = 'middle'>{todo.title}</td>
+
+                <td style={{ textAlign: 'right' }} valign = 'middle'>
 
               {/* ส่วน done waiting trash */}
                   {todo.completed ? (
@@ -170,20 +256,14 @@ function Todo() {
                       <span className='bi bi-check'></span>
                     </span>
                   ) : (
-                    <button className='btn btn-warning' onClick={() => {
-
-                      waitingClick(todo.id)
-
-                    }}>
+                    <button className='btn btn-warning' onClick={() =>waitingClick(todo.id)}>
                       waiting&nbsp;
                       <span className='bi bi-clock'></span>
                     </button>
                   )}
                   &nbsp;
                   {/* ปุ่มลบ */}
-                  <button className='btn btn-danger' onClick={ () => { 
-                    deleteClick(todo.id)
-                  } }>
+                  <button className='btn btn-danger' onClick={ () =>deleteClick(todo.id)}>
                     <span className='bi bi-trash'></span>
                   </button>
                 </td>
@@ -202,9 +282,7 @@ function Todo() {
             'todo-space btn ' +
             (curPage <= 1 ? 'btn-outline-secondary' : 'btn-outline-primary')
           }
-          onClick={() => {
-            setCurPage(1)
-          }}
+          onClick={() =>setCurPage(1)}
           disabled={curPage <= 1} //เมื่อหน้าเป็น 1 จะปิดการใช้งาน
         >
           First
@@ -249,9 +327,7 @@ function Todo() {
               ? 'btn-outline-secondary'
               : 'btn-outline-primary')
           }
-          onClick={() => {
-            setCurPage(numPages)
-          }}
+          onClick={() =>setCurPage(numPages)}
           disabled={curPage >= numPages}
         >
           Last
